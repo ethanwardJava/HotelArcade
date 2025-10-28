abbfb5fd697af0f5700e7953d3e8e2acfedce1f6
alireza <theethanwardhere@gmail.com>
2025-10-28 13:25:08 +0330

    MVN : ARCHETYPE CREATED THE PROJECT VIRSION 1.0.0

---
+++ src/main/java/com/arcade/hotelarcade/HotelArcadeApplication.java
@@ -0,0 +1,13 @@
+package com.arcade.hotelarcade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelArcadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelArcadeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcade/hotelarcade/HotelArcadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.hotelarcade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelArcadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
